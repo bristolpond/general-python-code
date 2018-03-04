30ce8930e30bd3ea5305355a6b02f30a3e92066a
murray_and_elena <murray_and_elena@hotmail.com>
2018-03-04 13:07:18 -0500

    updated to add a add class

--- Add.py
+++ Add.py
@@ -2,3 +2,12 @@ a = 1
 b = 2
 print 'a+b=',a+b
 
+class Add():
+  def __init__(self):
+    pass
+  
+  def add(self, var1, var2):
+    return var1+var2
+
+aclass = Add()
+print '1+2=',aclass.add(1,2)
